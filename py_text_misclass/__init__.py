436a7a6ec8462bf2475f10a72fd9befb875c9acf
Laura McLoughlin <lauramccomish@hotmail.com>
2020-08-17 14:33:24 +0100

    update

--- py_text_misclass/__init__.py
+++ py_text_misclass/__init__.py
@@ -1,9 +1,9 @@
-from py_text_misclass_multi_v1.py_text_misclass.misclass import Misclass
-from py_text_misclass_multi_v1.py_text_misclass.user_input_misclass import User_Input_Misclass
-import py_text_misclass_multi_v1.py_text_misclass.build
-import py_text_misclass_multi_v1.py_text_misclass.page_content
-import py_text_misclass_multi_v1.py_text_misclass.analysis_content
-import py_text_misclass_multi_v1.py_text_misclass.visualisations
+from py_text_misclass.misclass import Misclass
+from py_text_misclass.user_input_misclass import User_Input_Misclass
+import py_text_misclass.build
+import py_text_misclass.page_content
+import py_text_misclass.analysis_content
+import py_text_misclass.visualisations
 
 name = "py_text_misclass"
 __version__ = "0.0.1"
